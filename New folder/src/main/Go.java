e93c4a6bb25b44ac66f20fb7f7baeb48d731d8dc
Mrybalko173 <rybalkomihan@mail.ru>
2017-04-25 14:05:48 +0300

    Task1 0.2

--- New folder/src/main/Go.java
+++ New folder/src/main/Go.java
@@ -1,5 +1,40 @@
 package main;
 
 public class Go {
-
+	
+	public static int [] arrNew;
+	public static int [] arrBubble;
+	public static int [] arrInsert;
+	public static int [] arrSelect;
+	
+	public static void main (String[] args) throws Exception {
+		
+		MakeArray Array1 = new MakeArray();
+		arrNew = Array1.getArray();
+		arrBubble = arrNew.clone();
+		arrInsert = arrNew.clone();
+		arrSelect = arrNew.clone();
+		
+		System.out.println("New created array is");
+		Print.print(arrNew);
+		System.out.println();
+			
+		Sort sort = new Sort();
+		
+		System.out.println("Array after bubble sort algorythm");
+		sort.bubbleSort(arrBubble);
+		Print.print(arrBubble);
+		System.out.println();
+		
+		System.out.println("Array after insertion sort algorythm");
+		sort.insertionSort(arrInsert);
+		Print.print(arrInsert);
+		System.out.println();
+		
+		System.out.println("Array after selection sort algorythm");
+		sort.selectionSort(arrSelect);
+		Print.print(arrSelect);
+		System.out.println();
+		
+	}
 }
--- New folder/src/main/MakeArray.java
+++ New folder/src/main/MakeArray.java
@@ -1,5 +1,25 @@
 package main;
 
 public class MakeArray {
-
+	
+	int [] a = new int[Input.input()];
+	public MakeArray() throws Exception {
+		this.mArray();
+	}
+	
+	/**
+	 * @throws Exception 
+	 * 
+	 */
+	public void mArray () throws Exception {
+		Input.input();
+		
+		for (int i = 0; i<this.a.length;i++){
+			this.a[i] = (int)(Math.random()*100);
+		}
+	}
+	
+	public int[] getArray(){
+		return this.a;
+	} 
 }
