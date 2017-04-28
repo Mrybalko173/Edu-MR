d6ae0b27101ddc251a54b34375a5a03eafea53ed
Mrybalko173 <rybalkomihan@mail.ru>
2017-04-28 10:32:51 +0300

    Task4 0.2

--- Task4/src/main/Main.java
+++ Task4/src/main/Main.java
@@ -1,10 +1,14 @@
 package main;
 
+/**Main class
+ * @author mikhail.rybalko
+ *
+ */
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-		Minimum min = new Minimum();
-		min.minGet();
+		Minimum min = new Minimum(); //Create object of Minimum type
+		min.minGet(); //execute method for finding minimum of 4 numbers
 	}
 
 }
--- Task4/src/main/Minimum.java
+++ Task4/src/main/Minimum.java
@@ -1,5 +1,9 @@
 package main;
 
+/**Class of finding minimum
+ * @author mikhail.rybalko
+ *
+ */
 public class Minimum {
 	
 	int num1;
@@ -10,28 +14,28 @@ public class Minimum {
 	public void minGet() throws Exception{
 	
 	System.out.println("Enter first number");
-	num1 = Input.input();
+	num1 = Input.input(); 	// Input first value
 	System.out.println("Enter second number");
-	num2 = Input.input();
+	num2 = Input.input(); 	// Input second value
 	System.out.println("Enter third number");
-	num3 = Input.input();
+	num3 = Input.input(); 	// Input third value
 	System.out.println("Enter fourth number");
-	num4 = Input.input();
+	num4 = Input.input();	// Input fourth value
 	System.out.println("The smallest number is: " + min4());
 	
 	}
 	
-	public int min2(){ // 4.1
+	public int min2(){ // finding max of 2 method
 		int minNum = (num1 < num2)? num1 : num2;
 		return minNum;
 	}
 	
-	public int min3(){ // 4.2
+	public int min3(){ // finding max of 3 method
 		int minNum = (min2()<num3) ? min2(): num3;
 		return minNum;
 	}
 	
-	public int min4(){ // 4.3
+	public int min4(){ // finding max of 4 method
 		int minNum = (min3()<num4)? min3(): num4;
 		return minNum;
 	}
